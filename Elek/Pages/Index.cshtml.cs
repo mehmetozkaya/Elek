acba1a71db9b234745e4001eda6a03db7050c0f7
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-25 14:06:02 +0300

    Edit razor pages

--- Elek/Pages/Index.cshtml.cs
+++ Elek/Pages/Index.cshtml.cs
@@ -2,16 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Elek.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Elek.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly AppDbContext _db;
+
+        public IndexModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IList<Customer> Customers { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            Customers = await _db.Customers.AsNoTracking().ToListAsync();
+        }
+
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var contact = await _db.Customers.FindAsync(id);
+
+            if (contact != null)
+            {
+                _db.Customers.Remove(contact);
+                await _db.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
         }
     }
 }
