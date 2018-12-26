ac8295920002361193d4271dd4ad07d33cccfc45
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-26 11:51:33 +0300

    edit

--- Elek/Data/Movie.cs
+++ Elek/Data/Movie.cs
@@ -12,26 +12,19 @@ namespace Elek.Data
         public int ID { get; set; }
 
         [StringLength(60, MinimumLength = 3)]
-        [Required]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        [Range(1, 100)]
-        [DataType(DataType.Currency)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1, 100), DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Required]
-        [StringLength(30)]
-        public string Genre { get; set; }
-
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
         public string Rating { get; set; }
     }
 }
