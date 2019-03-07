64611dc6cd5e2eaf18397937c4448650056d085c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-07 16:05:33 +0300

    ElekInitialTestMain

---
+++ Elek.Test/ElekInitialTestMain.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Elek.Test
+{
+    [TestClass]
+    public class ElekInitialTestMain
+    {
+        [TestMethod]
+        public void ElekInitialTest()
+        {
+        }
+    }
+}
